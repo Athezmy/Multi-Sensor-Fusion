eb6d03362fdf86c57a16f6633e51af4bb85f7bc4
Yang Zhou <yungzhou@qq.com>
2021-02-18 15:01:15 +0800

    03-mapping-and-matching added

---
+++ 03-mapping-and-matching/lidar_localization/include/lidar_localization/sensor_data/gnss_data.hpp
@@ -0,0 +1,52 @@
+/*
+ * @Description: 
+ * @Author: Ren Qian
+ * @Date: 2019-07-17 18:25:13
+ */
+
+
+#ifndef LIDAR_LOCALIZATION_SENSOR_DATA_GNSS_DATA_HPP_
+#define LIDAR_LOCALIZATION_SENSOR_DATA_GNSS_DATA_HPP_
+
+#include <deque>
+#include <GeographicLib/LocalCartesian.hpp>
+
+namespace lidar_localization {
+class GNSSData {
+  public:
+    double time = 0.0;
+    double longitude = 0.0;
+    double latitude = 0.0;
+    double altitude = 0.0;
+
+    double local_E = 0.0;
+    double local_N = 0.0;
+    double local_U = 0.0;
+
+    //0s
+    double latitude_init =  48.9827;
+    double longitude_init =  8.39045;
+    double altitude_init = 116.396;
+    
+    double local_E_init = 0.0;
+    double local_N_init = 0.0;
+    double local_U_init = 0.0;
+
+    int status = 0;
+    int service = 0;
+
+    static double origin_longitude;
+    static double origin_latitude;
+    static double origin_altitude;
+
+  private:
+    static GeographicLib::LocalCartesian geo_converter;
+    static bool origin_position_inited;
+
+  public: 
+    void InitOriginPosition();
+    void UpdateXYZ();
+    static bool SyncData(std::deque<GNSSData>& UnsyncedData, std::deque<GNSSData>& SyncedData, double sync_time);
+};
+}
+#endif
